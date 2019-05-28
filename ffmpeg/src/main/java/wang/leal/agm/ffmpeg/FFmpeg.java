6e48c416aaf840133458fc27582122ee0e1d23c2
wangli <wangli@3commas.cn>
2019-05-28 15:13:23 +0800

    still error

--- ffmpeg/src/main/java/wang/leal/agm/ffmpeg/FFmpeg.java
+++ ffmpeg/src/main/java/wang/leal/agm/ffmpeg/FFmpeg.java
@@ -3,6 +3,12 @@ package wang.leal.agm.ffmpeg;
 public class FFmpeg {
 
     static {
+        System.loadLibrary("avutil");
+        System.loadLibrary("avcodec");
+        System.loadLibrary("swresample");
+        System.loadLibrary("avformat");
+        System.loadLibrary("swscale");
+        System.loadLibrary("avfilter");
         System.loadLibrary("ffmpeg");
     }
 
