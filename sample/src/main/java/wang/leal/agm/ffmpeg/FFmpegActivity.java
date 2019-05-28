ca626c13cb8db7f310fa4f7bd0edc03b2077362a
wangli <wangli@3commas.cn>
2019-05-28 14:29:40 +0800

    still error

--- sample/src/main/java/wang/leal/agm/ffmpeg/FFmpegActivity.java
+++ sample/src/main/java/wang/leal/agm/ffmpeg/FFmpegActivity.java
@@ -1,11 +1,18 @@
 package wang.leal.agm.ffmpeg;
 
+import android.Manifest;
 import android.os.Bundle;
 import android.text.method.ScrollingMovementMethod;
+import android.util.Log;
 import android.widget.TextView;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+
+import com.tbruyelle.rxpermissions2.RxPermissions;
+
+import java.io.File;
+import java.io.IOException;
 
 import wang.leal.agm.R;
 
@@ -15,10 +22,31 @@ public class FFmpegActivity extends AppCompatActivity {
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_ffmpeg);
+        final RxPermissions rxPermissions = new RxPermissions(this);
+        rxPermissions
+                .request(Manifest.permission.WRITE_EXTERNAL_STORAGE,Manifest.permission.READ_EXTERNAL_STORAGE)
+                .subscribe();
         TextView tvInfo = findViewById(R.id.tv_info);
         tvInfo.setMovementMethod(ScrollingMovementMethod.getInstance());
         findViewById(R.id.bt_execute).setOnClickListener(v -> {
-            tvInfo.setText("result:"+FFmpeg.execute("abc"));
+            String input = "/sdcard/DCIM/1558508952.mp4";
+            String output = "/sdcard/123456789.mp4";
+            File outputFile = new File(output);
+            if (!outputFile.exists()){
+                try {
+                    outputFile.createNewFile();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+//            tvInfo.setText("result:"+FFmpeg.execute("ffmpeg","-1",input,"-vf","crop=iw/2:ih/2",output));
+            new Thread(){
+                @Override
+                public void run() {
+                    int progress = FFmpeg.execute("ffmpeg","-i",input);
+                    Log.e("FFmpegActivity","progress:"+progress);
+                }
+            }.start();
         });
     }
 }
