338e6a0b50019261c6581369eb2710535b8c8bcc
wangli <wangli@3commas.cn>
2019-12-12 17:20:26 +0800

    delete arouter

--- sample/src/main/java/wang/leal/agm/AGMApplication.java
+++ sample/src/main/java/wang/leal/agm/AGMApplication.java
@@ -2,13 +2,10 @@ package wang.leal.agm;
 
 import android.app.Application;
 
-import com.alibaba.android.arouter.launcher.ARouter;
-
 public class AGMApplication extends Application {
 
     @Override
     public void onCreate() {
         super.onCreate();
-        ARouter.init(this);
     }
 }
